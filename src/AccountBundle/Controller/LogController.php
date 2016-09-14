b46459f71d08c10b8bb694e59121e3abe83c9969
gaston <saboy.gaston@gmail.com>
2016-09-14 09:22:17 +0200

    Ouech le sang

---
+++ src/AccountBundle/Controller/LogController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AccountBundle\Controller;
+
+use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use SiteBundle\Entity\Annonce;
+use SiteBundle\Entity\Categories;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogController extends Controller {
+
+    /**
+     * @Route("/connexion")
+     */
+    public function connecAction() {
+
+
+        /*
+          $repository = $this
+          ->getDoctrine()
+          ->getManager()
+          ->getRepository('AccountBundle:connexion');
+
+         */
+
+                if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+
+      return $this->redirectToRoute('Marquis');
+
+    }
+
+    // Le service authentication_utils permet de récupérer le nom d'utilisateur
+
+    // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+
+    // (mauvais mot de passe par exemple)
+
+    $authenticationUtils = $this->get('security.authentication_utils');
+
+
+    return $this->render('AccountBundle:Default:login.html.twig', array(
+
+      'last_username' => $authenticationUtils->getLastUsername(),
+
+      'error'         => $authenticationUtils->getLastAuthenticationError(),
+
+    ));
+
+
+       
+    }
+
+}
--- src/mainBundle/Controller/DefaultController.php
+++ src/mainBundle/Controller/DefaultController.php
@@ -4,14 +4,36 @@ namespace mainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AccountBundle\Entity\User;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     * @Route("/")
+     * @Route("/Marquis" , name="Marquis")
      */
-    public function indexAction()//Affichage de la page principale//
-    {
+    public function indexAction() {//Affichage de la page principale//
+        /*
+        $test = new User();
+        $em = $this->getDoctrine()->getManager();
+        
+        $test->setUsername("gast34");
+        $test->setPassword("5925c49719d5");
+        $test->setSalt("");
+        $test->setRoles(array("ROLE_USER"));
+        
+        $em->persist($test);
+        $em->flush();
+        
+        */
+        
+        
+        
+        
+        
+        
+
+
         return $this->render('mainBundle:Default:index.html.twig');
     }
+
 }
