0232f42d7b67dee8556dd25e859c66082ef9019f
Heijira <vince.feger@gmail.com>
2016-09-22 15:22:27 +0200

    function scroll

--- web/script/main.js
+++ web/script/main.js
@@ -46,3 +46,13 @@ $(function(){
          .removeAttr('checked')
          .removeAttr('selected');
 });
+
+
+  $(function(){
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+      var page = $(this).attr('href'); // Page cible
+      var speed = 1000; // Durée de l'animation (en ms)
+      $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+      return false;
+    });
+    
